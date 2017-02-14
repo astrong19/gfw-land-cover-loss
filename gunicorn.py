bb6c27ac1ed81ef8ac5ceb11f964bee680661324
astrong19 <astrong@wri.org>
2017-02-14 13:45:58 -0500

    change more ports

--- gunicorn.py
+++ gunicorn.py
@@ -1,7 +1,7 @@
 import os
 import multiprocessing
 
-bind = '0.0.0.0:5700'
+bind = '0.0.0.0:62141'
 backlog = 2048
 
 worker_class = 'gevent'
--- landcoverloss/config/base.py
+++ landcoverloss/config/base.py
@@ -6,7 +6,7 @@ settings={
     },
     'service': {
         'name': 'gfw-land-cover-loss',
-        'uri': 'http://172.30.2.76:5700',
-        'port': 5700
+        'uri': 'http://172.30.2.76:62141',
+        'port': 62141
     }
 }
