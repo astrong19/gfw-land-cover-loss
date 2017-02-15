ba6ef608ff05c8ea2402a7059ebe18b12c8161e3
astrong19 <astrong@wri.org>
2017-02-15 12:34:38 -0500

    add rendering rule

--- landcoverloss/routes/api/v1/ms_router.py
+++ landcoverloss/routes/api/v1/ms_router.py
@@ -23,9 +23,14 @@ def make_request():
 
     mosaic_rule = '{"mosaicMethod":"esriMosaicLockRaster","lockRasterIds":[1],"ascending":true,"mosaicOperation":"MT_FIRST"}'
 
-    #how does vizz grab the geoetry?
+    rendering_rule = '{"rasterFunction":"Arithmetic","rasterFunctionArguments":{"Raster":{"rasterFunction":"Remap","rasterFunctionArguments":{"InputRanges":[0,30,30,101],"OutputValues":[0,1],"Raster":"$2","AllowUnmatched":false}},"Raster2":{"rasterFunction":"Arithmetic","rasterFunctionArguments":{"Raster":{"rasterFunction":"Arithmetic","rasterFunctionArguments":{"Raster":{"rasterFunction":"Remap","rasterFunctionArguments":{"InputRanges":[1,16],"OutputValues":[16],"Raster":"$1","AllowUnmatched":false}},"Raster2":"$1","Operation":3}},"Raster2":"$3","Operation":1},"outputPixelType":"U8"},"Operation":3}}'
 
-    url = 'http://gis-gfw.wri.org/arcgis/rest/services/image_services/tree_cover_loss_year_wgs84/ImageServer/computeHistograms?geometry={0}&geometryType={1}&mosaicRule={2}&f=json'.format(geometry, shape, mosaic_rule)
+    #how does vizz grab the geoetry?
+    #need to send the geostore id to the microservice and get back the geojson
+    #/geostore/id
+    #example: https://github.com/resource-watch/adapter-earth-engine/blob/master/adapterearthengine/utils/http.py
+
+    url = 'http://gis-gfw.wri.org/arcgis/rest/services/image_services/tree_cover_loss_year_wgs84/ImageServer/computeHistograms?geometry={0}&geometryType={1}&renderingRule={2}&f=json'.format(geometry, shape, mosaic_rule)
 
     resp = requests.get(url=url)
     data = resp.json()
